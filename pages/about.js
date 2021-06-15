3c12edda51e969a17c48f28d1fa79652d5686a8c
Justine <hell.justine@gmail.com>
2021-06-15 11:37:47 +0200

    Custom Document & Head title for About page

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,11 @@
+import Head from 'next/head'
+
 const About = () => {
     return (
         <div>
+            <Head>
+                <title>About Us</title>
+            </Head>
             <h1>About</h1>
         </div>
     )
